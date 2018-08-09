6de415260ad5974cc2b491b0539a3bea5be049be
ImTaeYeong <imtaebari@gmail.com>
2018-08-09 17:16:18 +0900

    삭제기능 완료

--- app/src/main/java/com/ambit/otgorithm/adapters/DailyLookAdapter.java
+++ app/src/main/java/com/ambit/otgorithm/adapters/DailyLookAdapter.java
@@ -1,6 +1,8 @@
 package com.ambit.otgorithm.adapters;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
@@ -10,12 +12,15 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ambit.otgorithm.R;
 import com.ambit.otgorithm.dto.GalleryDTO;
 import com.ambit.otgorithm.modules.AnimationUtil;
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -29,6 +34,8 @@ public class DailyLookAdapter extends RecyclerView.Adapter<DailyLookAdapter.MyVi
     int previousPosition = 0;
     /************************************/
     FirebaseAuth mAuth;
+    FirebaseDatabase mFirebaseDb;
+    DatabaseReference mGalleryRef;
 
     // 생성자
     public DailyLookAdapter() { }
@@ -46,6 +53,8 @@ public class DailyLookAdapter extends RecyclerView.Adapter<DailyLookAdapter.MyVi
         MyViewHolder holder = new MyViewHolder(view);
         /************************************/
         mAuth = FirebaseAuth.getInstance();
+        mFirebaseDb = FirebaseDatabase.getInstance();
+        mGalleryRef = mFirebaseDb.getReference("galleries");
         /************************************/
         return holder;
     }
@@ -55,7 +64,8 @@ public class DailyLookAdapter extends RecyclerView.Adapter<DailyLookAdapter.MyVi
         holder.textView.setText(dailyLookList.get(position).sysdate);
         Uri uri = Uri.parse(dailyLookList.get(position).imageUrl);
         Glide.with(context).load(uri).into(holder.imageView);
-
+        holder.likey.setVisibility(View.INVISIBLE);
+        holder.star.setVisibility(View.INVISIBLE);
         if (position > previousPosition) {      // // We are scrolling DOWN
             AnimationUtil.animate(holder, true);
         } else {    // We are scrolling UP
@@ -65,9 +75,68 @@ public class DailyLookAdapter extends RecyclerView.Adapter<DailyLookAdapter.MyVi
 
         // add()
         final int currentPosition = position;
-        final GalleryDTO infoData = dailyLookList.get(position);
-    }
+        final GalleryDTO infoData = dailyLookList.get(currentPosition);
 
+        final CharSequence[] oitems = {"삭제","취소"};
+        final AlertDialog.Builder oDialog = new AlertDialog.Builder(context,android.R.style.Theme_DeviceDefault_Dialog_Alert);
+
+        if(dailyLookList.get(position).email.equals(mAuth.getCurrentUser().getEmail())){
+            holder.imageView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    oDialog.setTitle("").setItems(oitems, new DialogInterface.OnClickListener() {
+                        boolean isCanceled = false;
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if(!isCanceled && which == 0){
+                                oDialog.setMessage("정말로 삭제하시겠습니까?")
+                                        .setTitle("삭제")
+                                        .setPositiveButton("예", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                Toast.makeText(context,"삭제",Toast.LENGTH_SHORT).show();
+                                                mGalleryRef.child(infoData.gid).removeValue();
+                                                removeItem(infoData);
+                                            }
+                                        })
+                                        .setNeutralButton("아니오", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                isCanceled = true;
+                                                dialog.dismiss();
+                                                dialog.cancel();
+                                            }
+                                        }).show();
+                            }
+
+                            if(which == 1)
+                                Toast.makeText(context,"하이2",Toast.LENGTH_SHORT).show();
+
+                            dialog.dismiss();
+                        }
+                    }).show();
+
+                    return true;
+                }
+            });
+        }else {
+            holder.imageView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    Toast.makeText(context,"바이",Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+            });
+        }
+
+
+
+    }
+    private void removeItem(GalleryDTO infoData) {
+        int CurrPosition = dailyLookList.indexOf(infoData);
+        dailyLookList.remove(CurrPosition);
+        notifyItemRemoved(CurrPosition);
+    }
     @Override
     public int getItemCount() {
         return dailyLookList.size();
