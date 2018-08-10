945a91fa0b9e8b36d236404402db7402a0ba3105
CoRock <dycorphin@gmail.com>
2018-08-10 23:10:11 +0900

    update circle indicator and SettingsActivity

--- app/src/main/java/com/ambit/otgorithm/views/MainActivity.java
+++ app/src/main/java/com/ambit/otgorithm/views/MainActivity.java
@@ -25,7 +25,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -41,12 +40,10 @@ import android.widget.Toast;
 
 import com.ambit.otgorithm.R;
 import com.ambit.otgorithm.adapters.AutoScrollAdapter;
-import com.ambit.otgorithm.dto.UserDTO;
 import com.ambit.otgorithm.models.Common;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.facebook.login.LoginManager;
-import com.google.android.gms.ads.MobileAds;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -55,7 +52,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.iid.FirebaseInstanceId;
-import com.igalata.bubblepicker.rendering.Item;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -72,6 +68,7 @@ import java.util.List;
 import java.util.Map;
 
 import cn.trinea.android.view.autoscrollviewpager.AutoScrollViewPager;
+import me.relex.circleindicator.CircleIndicator;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
 
@@ -81,6 +78,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     private static TextView tv;
 
     private AutoScrollViewPager autoViewPager;
+    private CircleIndicator mIndicator;
+
     AutoScrollAdapter autoScrollAdapter;
 
     // 툴바 변수 선언
@@ -204,20 +203,15 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     String nickName = dataSnapshot.getValue(String.class);
                     if(nickName == null){
-                        Intent intent = new Intent(MainActivity.this, InputNameActivity.class);
+                        Intent intent = new Intent(MainActivity.this, AddInfoActivity.class);
                         startActivity(intent);
                     }
                 }
 
                 @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
+                public void onCancelled(DatabaseError databaseError) { }
             });
         }
-
-
-
 
         // 도움말; a는 마치 File 이름
         SharedPreferences preference = getSharedPreferences("a", MODE_PRIVATE);
@@ -249,17 +243,13 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         /****************************************************************/
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.main_content);
 
         final NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         View nav_header_view = navigationView.getHeaderView(0);
         Menu menu = navigationView.getMenu();
-        if(mFirebaseUser != null){
-            menu.findItem(R.id.nav_aboutUs_logout).setVisible(true);
-        }else {
-            menu.findItem(R.id.nav_aboutUs_logout).setVisible(false);
-        }
-
+        if (mFirebaseUser != null) { menu.findItem(R.id.nav_aboutUs_logout).setVisible(true); }
+        else { menu.findItem(R.id.nav_aboutUs_logout).setVisible(false); }
 
         // nav_header의 layout을 누르면 로그인 화면으로 넘어가는 부분
         navToSignIn = nav_header_view.findViewById(R.id.to_sign_in);
@@ -329,6 +319,11 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                             Toast.makeText(MainActivity.this, "로그인을 해야 이용가능합니다", Toast.LENGTH_SHORT).show();
                         }
                         // Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.nav_aboutUs_settings:
+                        intent = new Intent(MainActivity.this, SettingActivity.class);
+                        startActivity(intent);
                         break;
 
                     case R.id.nav_aboutUs_intro:
@@ -420,9 +415,13 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         bannerList.add("http://13.125.253.250/banners/banner3.png");
 
         autoViewPager = (AutoScrollViewPager) findViewById(R.id.autoViewPager);
+        mIndicator = (CircleIndicator) findViewById(R.id.main_indicator);
+
         // AutoScrollAdapter에 사진을 담은 arrayList 전달
         AutoScrollAdapter scrollAdapter = new AutoScrollAdapter(getLayoutInflater(), bannerList, MainActivity.this);
+
         autoViewPager.setAdapter(scrollAdapter);    // Auto Viewpager에 Adapter 장착
+        mIndicator.setViewPager(autoViewPager);
         autoViewPager.setInterval(5000);            // 페이지 넘어갈 시간 간격 설정
         autoViewPager.startAutoScroll();            // Auto Scroll 시작
 
---
+++ app/src/main/java/com/ambit/otgorithm/views/SettingActivity.java
@@ -0,0 +1,144 @@
+package com.ambit.otgorithm.views;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.ambit.otgorithm.R;
+
+public class SettingActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+
+    Toolbar toolbar;
+    TextView textViewToolbarTitle;
+
+    private static final String TAG = "PreSettingsActivity";
+
+    private static final String USE_USER_NAME = "key_useUserName";
+    private static final String USER_NAME = "key_userName";
+    private static final String USE_BACKGROUND_COLOR = "key_backgroundcolor";
+    private static final String BACKGROUND_COLOR = "key_dialog_backgroundcolor";
+    private static final String TEXT_COLOR = "key_textcolor";
+    private static final String ALL_REMOVE_MEMO = "key_all_memo_clear";
+
+    private PreferenceScreen screen;
+    private CheckBoxPreference mUseUsername;
+    private EditTextPreference mUsername;
+    private ListPreference mbackgroundcolor;
+    private ListPreference mTextcolor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.settings_toolbar);
+
+        textViewToolbarTitle = (TextView) findViewById(R.id.toolbar_title);
+        textViewToolbarTitle.setText("설정");
+        textViewToolbarTitle.setGravity(View.TEXT_ALIGNMENT_CENTER);
+        textViewToolbarTitle.setTextColor(Color.WHITE);
+
+        /*toolbar.setTitle("설정");
+        toolbar.setTitleTextColor(Color.WHITE);*/
+        //setGravity(View.TEXT_ALIGNMENT_CENTER);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //
+        addPreferencesFromResource(R.xml.pref_settings);
+
+
+
+        screen = getPreferenceScreen();
+
+        //인자로 전달되는 Key값을 가지는 Preference 항목의 인스턴스를 가져옴
+        //굳이 여러곳에서 사용하지 않는 이상에는 이런식으로 객체화 시킬필요는 없는듯
+        mUsername = (EditTextPreference) screen.findPreference(USER_NAME);
+        mbackgroundcolor = (ListPreference) screen.findPreference(BACKGROUND_COLOR);
+        mTextcolor = (ListPreference) screen.findPreference(TEXT_COLOR);
+
+        //변화 이벤트가 일어났을 시 동작
+        mUsername.setOnPreferenceChangeListener(this);
+        mbackgroundcolor.setOnPreferenceChangeListener(this);
+        mTextcolor.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        updateSummary();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+                                         Preference preference) {
+        Log.i(TAG, "onPreferenceTreeClick");
+        if(preference.getKey().equals("key_all_memo_clear")){
+            Log.i(TAG, "key_all_memo_clear");
+            //모든 메모 삭제 기능 넣을 것
+//          showRemoveMemoDialog();
+        }
+        return false;
+    }
+
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Log.i(TAG, "preference : " + preference +", newValue : "+ newValue);
+
+        String value = (String) newValue;
+        if(preference == mUsername){
+            Log.i(TAG, "mUsername onPreferenceChange");
+            mUsername.setSummary(value);
+        }else if(preference == mbackgroundcolor){
+            ListPreference listPreference = (ListPreference) preference;
+            int index = listPreference.findIndexOfValue(value);
+            mbackgroundcolor.setSummary(index >= 0 ? listPreference.getEntries()[index]
+                    : null);    // entries 값 대신 이에 해당하는 entryValues값 set
+        }else if(preference == mTextcolor){
+            ListPreference listPreference = (ListPreference) preference;
+            int index = listPreference.findIndexOfValue(value);
+            mTextcolor.setSummary(index >= 0 ? listPreference.getEntries()[index]
+                    : null);    // entries 값 대신 이에 해당하는 entryValues값 set
+        }
+        return true;
+    }
+
+
+    private void updateSummary(){
+        //액티비티 실행 할 때 저장되어있는 summary값을 set
+        //안 하면 안 뜸
+        mUsername.setSummary(mUsername.getText());
+        mbackgroundcolor.setSummary(mbackgroundcolor.getEntry());
+        mTextcolor.setSummary(mTextcolor.getEntry());
+        Log.d(TAG,"mbackgroundcolor="+mbackgroundcolor +", mUsername :" + mUsername);
+    }
+
+    /*    @Override
+    public void setContentView(int layoutResID) {
+        ViewGroup contentView = (ViewGroup) LayoutInflater.from(this).inflate(
+                R.layout.activity_setting, new LinearLayout(this), false);
+
+        toolbar = (Toolbar) contentView.findViewById(R.id.toolbar_basic);
+
+        ViewGroup contentWrapper = (ViewGroup) contentView.findViewById(R.id.content);
+        LayoutInflater.from(this).inflate(layoutResID, contentWrapper, true);
+
+        getWindow().setContentView(contentView);
+    }*/
+
+}
